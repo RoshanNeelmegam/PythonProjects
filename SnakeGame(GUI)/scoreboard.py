77d938ac845fb552d795c43a5961083243da9294
RoshanNeelmegam <98408299+RoshanNeelmegam@users.noreply.github.com>
2022-07-02 16:50:32 +0530

    Create scoreboard.py

---
+++ SnakeGame(GUI)/scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+      def __init__(self):
+            super().__init__()
+            self.score = 0
+            self.color("white")
+            self.hideturtle()
+            self.penup()
+            self.goto(0, 270)
+            self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+ 
+      def score_update(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+      
+      def collisionWithFood(self, snake, food):
+           print(snake.snake_body[0].distance(food))
+           if snake.snake_body[0].distance(food) <= 21:
+             food.update()
+             self.score_update()
+
+      def collisionWithWall(self, snake):
+          if snake.snake_body[0].xcor() > 295 or snake.snake_body[0].xcor() < -295 or snake.snake_body[0].ycor() > 295 or snake.snake_body[0].ycor() < -295:
+            self.clear()
+            self.goto(0, 0)
+            self.write("Game Over", align="center", font=("Arial", 25, "normal"))
+            self.goto(0, -30)
+            self.write(f"Score is {self.score}", align="center", font=("Arial", 18, "normal"))
+            return '1'
+          else:
+            return '0'
