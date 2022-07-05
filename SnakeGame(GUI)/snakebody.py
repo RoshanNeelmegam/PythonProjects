5d3c370eda9e7d3e41be9098e02a3f0f7262c66f
RoshanNeelmegam <98408299+RoshanNeelmegam@users.noreply.github.com>
2022-07-05 21:48:59 +0530

    Update snakebody.py

--- SnakeGame(GUI)/snakebody.py
+++ SnakeGame(GUI)/snakebody.py
@@ -15,19 +15,28 @@ class Snake:
         for turtles in range(3):
             t1 = Turtle()
             t1.shape("square")
-            t1.color("green")
+            t1.color("white")
             t1.shapesize(1.0, 1.0, 0)
             t1.penup()
             t1.setpos(x, y)
-            t1.speed(6)
+            t1.speed("fastest")
             # since size of one turtle is 20 by default, shifting the blocks by 20.
             x -= 20
             self.snake_body.append(t1)
     
     # snake's function to move and extend it's body.
     def move(self):
-        for turtles in self.snake_body:
-            turtles.forward(20)
+        for i in range(len(self.snake_body), 0, -1):
+            if i == 1:
+                # skipping the below lines as head of the snake is anyways moved at the end.
+                # # The next line is important in the sense that it helps in keeping the bodies of the snake separate by moving the head.
+                # self.snake_body[0].forward(18)
+                pass
+            else:
+                self.snake_body[i-1].setheading(self.snake_body[i-2].heading())
+                self.snake_body[i-1].setpos(self.snake_body[i-2].xcor(), self.snake_body[i-2].ycor())
+        # This line is important as the head has to move before the other parts can be moved
+        self.snake_body[0].forward(18)
         
     def up(self):
         if self.snake_body[0].heading() == 90:
@@ -68,12 +77,26 @@ class Snake:
             pass
         else:
             self.snake_body[0].left(90)
-       
-    def update(self):
-        for i in range(len(self.snake_body), 0, -1):
-            if i == 1:
-                # The next line is important in the sense that it helps in keeping the bodies of the snake separate by moving the head.
-                self.snake_body[0].forward(20)
-            else:
-                self.snake_body[i-1].setheading(self.snake_body[i-2].heading())
-                self.snake_body[i-1].setpos(self.snake_body[i-2].xcor(), self.snake_body[i-2].ycor())
+        
+    def extend(self):
+        t1 = Turtle()
+        t1.shape("square")
+        t1.color("white")
+        t1.shapesize(1.0, 1.0, 0)
+        t1.penup()
+        t1.goto(self.snake_body[-1].position())
+        t1.speed("fastest")
+        self.snake_body.append(t1)
+
+
+      
+   
+
+    
+        
+
+    
+
+
+    
+
